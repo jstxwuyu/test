eaf30f591775c59e88693bca6c99746ac9a799ff
jstxwuyu <jstxwuyu@163.com>
2019-07-22 17:12:59 +0800

    练习中

---
+++ iota枚举.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	type jier int64
+	var a jier
+	var c jier
+	var x jier
+
+fmt.Printf("a=%v\n",a)
+println("c=",c)
+	print("x=")
+fmt.Scan(&x)
+a ++
+println("a=",a)
+println("a>x",a >= x)
+}
